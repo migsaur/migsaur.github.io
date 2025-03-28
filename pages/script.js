b75c70654a1bc6630dd3ebf5f5843a29d461b854
migsaur <134446725+migsaur@users.noreply.github.com>
2025-03-28 11:47:48 -0700

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -1,1 +1,11 @@
+const menuButton = document.getElementById('menuButton');
+const menuList = document.getElementById('navList');
+const menuElement = document.querySelectorAll('.navElement')
 
+menuButton.addEventListener('click', () => {
+  menuList.classList.toggle("hide");
+})
+
+menuElement.addEventListener('click', () => {
+  menuList.classList.toggle("hide");
+})
